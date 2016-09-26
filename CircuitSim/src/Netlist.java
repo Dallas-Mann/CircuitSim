d6434ac010503d3090a7e99e8c4974801da0549e
Dallas Mann <Dallas.mann94@gmail.com>
2016-09-25 23:36:43 -0600

    Initial implementation of solveFrequency Method.
    Need to read in type of simulation from file, and if it's a frequency
    solution I need to read in the frequency range and number of steps over
    that range.

--- CircuitSim/src/Netlist.java
+++ CircuitSim/src/Netlist.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
